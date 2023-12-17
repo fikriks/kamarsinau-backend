befde0bdb322acf618081f729e72b046894366d8
fikriks <fikriks130@gmail.com>
2023-12-17 11:38:47 +0700

    update: auth controller :rocket:

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const User = require("../models").User;
+const Course = require("../models").Course;
+const CourseStudent = require("../models").CourseStudent;
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("../config/config");
@@ -25,14 +27,22 @@ module.exports = {
         });
       }
 
-      const token = jwt.sign({ id: user.id }, config.secret, {
+      const token = jwt.sign({ 
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        level: user.level
+       }, config.secret, {
         expiresIn: 86400, // 24 hours
       });
 
       req.session.token = token;
+      req.session.user = user;
+
       return res.status(200).send({
         status: "success",
-        message: "User logged successfully",
+        message: "User retrieved",
         data: {
           accessToken: token,
         },
@@ -44,18 +54,70 @@ module.exports = {
 
   register: async (req, res) => {
     try {
-      const { email, password } = req.body;
+      const { name, email, password, level } = req.body;
 
-      await User.create({
+      const user = await User.create({
+        name: name,
         email: email,
         password: bcrypt.hashSync(password),
-        role: 'student'
+        role: "student",
+        level: level,
       });
 
-      return res.send({ 
-        status: 'success',
-        message: "User registered successfully!"
-     });
+      const courses = await Course.findAll({
+        where: {
+          level: level
+        }
+      });
+
+      if(courses.length == 0){
+        return res.status(404).send({ message: "Course Not found." });
+      }
+
+      courses.forEach(async (course) => {
+        await CourseStudent.create({
+            userId: user.id,
+            courseId: course.id
+          })
+      });
+
+      return res.send({
+        status: "success",
+        message: "User registered successfully!",
+      });
+    } catch (error) {
+      return res.status(500).send({ message: error.message });
+    }
+  },
+
+  me: async (req, res) => {
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+          return res
+            .status(401)
+            .json({ message: "Unauthorized: Missing or invalid token" });
+        }
+
+        const token = authHeader.split(" ")[1];
+
+       // Verify the token
+       jwt.verify(token, config.secret, (err, decoded) => {
+         if (err) {
+           return res
+             .status(401)
+             .json({ message: "Unauthorized: Invalid token" });
+         }
+
+         // Attach decoded user information to the request object
+         return res.send({
+           status: "success",
+           message: "User retrieved successfully!",
+           token: token,
+           data: decoded
+         });
+       });
     } catch (error) {
       return res.status(500).send({ message: error.message });
     }
