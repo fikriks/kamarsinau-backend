23c3976ad0c2f77448e48a7cd6e7ee08f34e733b
fikriks <fikriks130@gmail.com>
2023-12-17 11:38:34 +0700

    update: app :rocket:

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require("express-session");
+var cors = require("cors");
 
 const PREFIX = `/api/v1`;
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/users');
 const authRouter = require("./routes/auth");
 const courseRouter = require("./routes/course");
 const moduleRouter = require("./routes/module");
-const lessonRouter = require("./routes/lesson");
 const progressRouter = require("./routes/progress");
+const courseInstructorRouter = require("./routes/courseInstructor");
+const courseStudentRouter = require("./routes/courseStudent");
 
 var app = express();
+app.use(cors());
 
 app.use(
   session({
@@ -34,8 +37,9 @@ app.use('/', indexRouter);
 app.use(`${PREFIX}/`, authRouter);
 app.use(`${PREFIX}/courses`, courseRouter);
 app.use(`${PREFIX}/modules`, moduleRouter);
-app.use(`${PREFIX}/lessons`, lessonRouter);
-app.use(`${PREFIX}/lessons`, lessonRouter);
 app.use(`${PREFIX}/progress`, progressRouter);
+app.use(`${PREFIX}/users`, userRouter);
+app.use(`${PREFIX}/course-instructors`, courseInstructorRouter);
+app.use(`${PREFIX}/course-students`, courseStudentRouter);
 
 module.exports = app;
